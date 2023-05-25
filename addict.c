6fc07189bffde09afcf7e46f0e1f1b8e872815aa
Itsjustdice <ckdebruin@gmail.com>
2023-05-25 10:00:31 -0700

    addict.c

--- addict.c
+++ addict.c
@@ -8,6 +8,8 @@
  */
 int invlove(info_t *data)
 {
+	/* Verify that the file descriptor is within range and */
+	/* that the shell is reading from a terminal.*/
 	return (isatty(STDIN_FILENO) && data->readingFd <= 2);
 }
 
@@ -19,10 +21,11 @@ int invlove(info_t *data)
  */
 int we_believe(char c, char *limitme)
 {
+	/* Make sure the character corresponds to one of the string's delimiters */
 	while (*limitme)
 		if (*limitme++ == c)
 			return (1);
-	return (0);
+	return (0); /* 0 will be returned if the character is not a delimiter */
 }
 
 /**
@@ -34,9 +37,9 @@ int we_believe(char c, char *limitme)
 int isAlphabetic(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
+		return (1); /* the characters are alphabetical */
 	else
-		return (0);
+		return (0); /* The characters are not alphabetical */
 }
 
 /**
@@ -49,26 +52,28 @@ int convertToInteger(char *s)
 {
 	int a, tive = 1, onOff = 0, outta;
 	unsigned int answer = 0;
-
+	/* until the string reaches a null terminator or onOff is 2 */
+	/* iterate through the string */
 	for (a = 0;  s[a] != '\0' && onOff != 2; a++)
 	{
 		if (s[a] == '-')
-			tive *= -1;
+			tive *= -1; /* If a '-' appears, change the number's sign */
 
 		if (s[a] >= '0' && s[a] <= '9')
 		{
-			onOff = 1;
+			onOff = 1; /* Tell the user that genuine digits have been located */
 			answer *= 10;
-			answer += (s[a] - '0');
+			answer += (s[a] - '0'); /* Transform a character into a digit */
+			/* then add up the value */
 		}
 		else if (onOff == 1)
-			onOff = 2;
+			onOff = 2; /* Show that the string's numerical portion has ended */
 	}
-
+	/* Apply the sign to the transformed integer's final value */
 	if (tive == -1)
 		outta = -answer;
 	else
 		outta = answer;
 
-	return (outta);
+	return (outta); /* Provide the translated integer value back */
 }
