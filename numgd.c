bb7358d1dbeebeaa8f0c0f09866a26e73af46163
Itsjustdice <ckdebruin@gmail.com>
2023-05-25 10:57:36 -0700

    numgd.c

--- numgd.c
+++ numgd.c
@@ -10,15 +10,18 @@
 char *copyString(char *dest, char *place)
 {
 	int a = 0;
-
+	/* Check for exceptional circumstances, such as when the destination and */
+	/* the location are the same or when the location is NULL */
 	if (dest == place || place == 0)
 		return (dest);
+	/* Copying characters from one place to another until */
+	/* the null terminator is reached */
 	while (place[a])
 	{
 		dest[a] = place[a];
 		a++;
 	}
-	dest[a] = 0;
+	dest[a] = 0; /* the target string should have a null terminator added */
 	return (dest);
 }
 
@@ -32,14 +35,17 @@ char *_strdupsd(const char *txt)
 {
 	int length = 0;
 	char *ret;
-
+	/* make sure input string is NULL */
 	if (txt == NULL)
 		return (NULL);
+	/* Determine the input string's length */
 	while (*txt++)
 		length++;
+	/* Memory should be set aside for the repeated string */
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
 		return (NULL);
+	/* Reverse-transfer the input string's characters to the replica string */
 	for (length++; length--;)
 		ret[length] = *--txt;
 	return (ret);
@@ -55,11 +61,11 @@ void putin(char *txt)
 {
 	int a = 0;
 
-	if (!txt)
+	if (!txt) /* make sure input string is NULL */
 		return;
 	while (txt[a] != '\0')
 	{
-		_putchar(txt[a]);
+		_putchar(txt[a]); /* character needs to be printed */
 		a++;
 	}
 }
@@ -75,13 +81,14 @@ int _putchar(char c)
 {
 	static int a;
 	static char buffed[WRITE_BUFFER];
-
+	/* Verify the character's flushing behavior or whether the buffer is full */
 	if (c == BUFFER_FLUSHER || a >= WRITE_BUFFER)
 	{
-		write(1, buffed, a);
-		a = 0;
+		write(1, buffed, a); /* To standard output, send characters in the buffer */
+		a = 0; /* rearrange buffer index */
 	}
+	/* The character should not be a buffer flusher, so check */
 	if (c != BUFFER_FLUSHER)
-		buffed[a++] = c;
+		buffed[a++] = c; /* Fill the buffer with the character */
 	return (1);
 }
